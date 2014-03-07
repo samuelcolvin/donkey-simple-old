5b9ba7ed88bfee9db0e026d2b4b75ffe827400ac
samuelcolvin <s@muelcolvin.com>
2014-03-07 12:24:44 +0000

    changed structure to deployable module

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='DonkeySimple',
+      version='0.1',
+      description='Super Simple python system for building and deploying static websites',
+      author='Samuel Colvin',
+      author_email='S@muelColvin.com',
+      url='http://www.scolvin.com',
+      packages=['DonkeySimple', 'DonkeySimple.Build', 'DonkeySimple.WebInterface'],
+      package_data={'DonkeySimple': [
+        'site-template/*.*',
+        'site-template/static/*.*',
+        'site-template/edit/*.*',
+        'site-template/edit/repos/default/pages/*.*',
+        'site-template/edit/repos/default/static/*.*',
+        'site-template/edit/repos/default/templates/*.*',
+      ]},
+      scripts=['DonkeySimple/bin/donkey-simple.py']
+)

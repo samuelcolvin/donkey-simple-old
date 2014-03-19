5e12851a4ef9f5e0825a65788677ff7724ac72c8
samuelcolvin <s@muelcolvin.com>
2014-03-19 02:33:34 +0000

    incrementing version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('DonkeySimple/docs.txt') as file:
     long_description = file.read()
 
 setup(name='DonkeySimple',
-    version='0.16',
+    version='0.17',
     description='Super Simple python system for building and deploying static websites',
     long_description=long_description,
     author='Samuel Colvin',
